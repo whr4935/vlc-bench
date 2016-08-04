f292c802f454af847a7a09923115c7184f0f7810
Bastien Penavayre <bastienPenava@gmail.com>
2016-08-04 11:51:37 +0200

    Save/load lastTestInfo member in onSave/RestoreInstanceState()

--- VLCBenchmark/app/src/main/java/org/videolan/vlcbenchmark/TestPage.java
+++ VLCBenchmark/app/src/main/java/org/videolan/vlcbenchmark/TestPage.java
@@ -117,6 +117,7 @@ public class TestPage extends Activity implements BenchServiceListener {
         savedInstanceState.putDouble("SOFT_SCORE", softScore);
         savedInstanceState.putDouble("HARD_SCORE", hardScore);
         savedInstanceState.putSerializable("RESULTS_TEST", (Serializable) resultsTest);
+        savedInstanceState.putSerializable("LAST_TEST_INFO", lastTestInfo);
         super.onSaveInstanceState(savedInstanceState);
     }
 
@@ -130,6 +131,7 @@ public class TestPage extends Activity implements BenchServiceListener {
         loopNumber = savedInstanceState.getInt("CURRENT_LOOP_NUMBER");
         softScore = savedInstanceState.getDouble("SOFT_SCORE");
         hardScore = savedInstanceState.getDouble("HARD_SCORE");
+        lastTestInfo = (TestInfo) savedInstanceState.getSerializable("LAST_TEST_INFO");
         resultsTest = (List<TestInfo>[]) savedInstanceState.getSerializable("RESULTS_TEST");
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
     }
