97ed4292d197ba13cfae72c96043949e150d3a57
Bastien Penavayre <bastienPenava@gmail.com>
2016-08-05 14:42:19 +0200

    Add a title row to the ResultPage on top of the list
    Add a row over the list of tests that indicates what collum correspond to
    what information.

--- VLCBenchmark/app/src/main/java/org/videolan/vlcbenchmark/GridFragment.java
+++ VLCBenchmark/app/src/main/java/org/videolan/vlcbenchmark/GridFragment.java
@@ -1,11 +1,15 @@
 package org.videolan.vlcbenchmark;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import org.videolan.vlcbenchmark.R;
 import org.videolan.vlcbenchmark.ResultAdapter;
@@ -28,11 +32,25 @@ public class GridFragment extends Fragment {
 
     }
 
+    private void fillHeader(View view, int id, String text) {
+        TextView textView = (TextView) view.findViewById(id);
+        textView.setText(text);
+        textView.setTypeface(null, Typeface.BOLD);
+        textView.setTextColor(Color.BLACK);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_grid, container, false);
         ListView gv = (ListView)v.findViewById(R.id.resultList);
+        LayoutInflater rowInflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = rowInflater.inflate(R.layout.result_row, null);
+        fillHeader(rowView, R.id.fileName, "Video name");
+        fillHeader(rowView, R.id.softScore, "Software");
+        fillHeader(rowView, R.id.hardScore, "Hardware");
+        rowView.setMinimumHeight(0);
+        gv.addHeaderView(rowView);
         ResultAdapter resultAdapter = new ResultAdapter(getActivity());
         resultAdapter.setResults(results);
         gv.setAdapter(resultAdapter);
