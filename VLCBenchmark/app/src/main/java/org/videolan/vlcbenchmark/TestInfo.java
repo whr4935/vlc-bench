66d268d68f90b791cd9864f5e9948913bf559c2d
Bastien Penavayre <bastienPenava@gmail.com>
2016-08-12 12:15:33 +0200

    Duplicate data for hard/software case, correctifs, adds methods
    Make fields framesDropped, percentOfBadScreenshots, numberOfWarnings into
      array of two values the first for the software tests (SOFT constant), the other
      for the hardware tests (HARD constant).
    Adds a field "crashed" to save if vlc crashed during a certain test.
    Adds methods thant handle the scoring process.

--- VLCBenchmark/app/src/main/java/org/videolan/vlcbenchmark/TestInfo.java
+++ VLCBenchmark/app/src/main/java/org/videolan/vlcbenchmark/TestInfo.java
@@ -7,26 +7,51 @@ import java.io.Serializable;
  */
 public class TestInfo implements Serializable {
 
-    public static int PERFORMANCE = 0;
-    public static int PLAYBACK = 1;
+    public static final int PLAYBACK = 0; //screenshot
+    public static final int PERFORMANCE = 1; //bad frames
+    static final int SOFT = 0;
+    static final int HARD = 1;
+    private static final String PLAYBACK_STR = "playback (screenshots/seek)";
+    private static final String PERFORMANCE_STR = "performance (frames dropped)";
 
     String name;
-    double[] software = {30d, 20d}; //performance, playback
-    double[] hardware = {30d, 20d};
+    double[] software = {20d, 30d}; //playback, performance
+    double[] hardware = {20d, 30d};
     int loopNumber;
-    int framesDropped;
-    double percentOfBadScreenshots;
-    int numberOfWarnings;
+    int[] framesDropped = {0, 0};
+    double[] percentOfBadScreenshots = {0d, 0d};
+    int[] numberOfWarnings = {0, 0};
+    boolean[][] crashed = {{false, false}, {false, false}};
 
-    public void badScreenshot(boolean isSoftware) {
-//        (isSoftware ? software : hardware)[PLAYBACK];
+    public void badScreenshot(double percent, boolean isSoftware) {
+        percentOfBadScreenshots[isSoftware ? SOFT : HARD] = percent;
     }
 
     public void badFrames(int number_of_dropped_frames, boolean isSoftware) {
         double[] tmp = (isSoftware ? software : hardware);
 
+        framesDropped[isSoftware ? SOFT : HARD] += number_of_dropped_frames;
         tmp[PERFORMANCE] -= 5 * number_of_dropped_frames;
         if (tmp[PERFORMANCE] <= 0)
             tmp[PERFORMANCE] = 0;
+    }
+
+    public void vlcCrashed(boolean isSoftware, boolean isScreenshot) {
+        crashed[isSoftware ? SOFT : HARD][isScreenshot ? PLAYBACK : PERFORMANCE] = true;
+        (isSoftware ? software : hardware)[(isScreenshot ? PLAYBACK : PERFORMANCE)] = 0;
+    }
+
+    private static String strip(String str) {
+        if (str.startsWith("\n"))
+            str = str.substring(1);
+        if (str.endsWith("\n"))
+            str = str.substring(0, str.length());
+        return str;
+    }
+
+    public String getCrashes(int index) {
+        if (!crashed[index][PLAYBACK] && !crashed[index][PERFORMANCE])
+            return "";
+        return strip((crashed[index][PLAYBACK] ? PLAYBACK_STR : "") + "\n" + (crashed[index][PERFORMANCE] ? PERFORMANCE_STR : "") + '\n');
     }
 }
