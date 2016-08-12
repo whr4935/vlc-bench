9f5283a71fec318ed7d05a0d13f3d202a70d5fd3
Bastien Penavayre <bastienPenava@gmail.com>
2016-08-12 11:14:28 +0200

    Implement new layout result_detail as a Dialog's content view
    By doing so replacing the previous usage of AlertDialog.Builder
    with Dialog and preparing all the fields we'll use.

--- VLCBenchmark/app/src/main/java/org/videolan/vlcbenchmark/ResultAdapter.java
+++ VLCBenchmark/app/src/main/java/org/videolan/vlcbenchmark/ResultAdapter.java
@@ -1,10 +1,11 @@
 package org.videolan.vlcbenchmark;
 
-import android.app.AlertDialog;
+import android.app.Dialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
@@ -41,24 +42,37 @@ public class ResultAdapter extends BaseAdapter {
         TextView tv2 = (TextView) view.findViewById(R.id.softScore);
         TextView tv3 = (TextView) view.findViewById(R.id.hardScore);
 
-        TestInfo test = results.get(i);
+        final TestInfo test = results.get(i);
         tv1.setText(test.name);
         tv2.setText(String.valueOf(test.software[0] + test.software[1]));
         tv3.setText(String.valueOf(test.hardware[0] + test.hardware[1]));
 
         view.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                final Dialog dialog = new Dialog(v.getContext());
+                dialog.setContentView(R.layout.result_detail);
+                dialog.setTitle("Details for\n" + test.name);
+                TextView tmp;
 
-                new AlertDialog.Builder(v.getContext())
-                        .setTitle("Test details")
-                        .setMessage("Test name : " + results.get(i).name
-                                + "\nFrames dropped : " + doubleToPercentString(results.get(i).percentOfFrameDrop)
-                                + "\nBad screenshots : " + doubleToPercentString(results.get(i).percentOfBadScreenshots)
-                                + "\nBad seeks : " + doubleToPercentString(results.get(i).percentOfBadSeek)
-                                + "\nWarnings : " + results.get(i).numberOfWarnings
-                        )
-                        .setPositiveButton(android.R.string.yes, null)
-                        .show();
+                tmp = (TextView) dialog.findViewById(R.id.frames_dropped_software);
+                tmp = (TextView) dialog.findViewById(R.id.frames_dropped_hardware);
+
+                tmp = (TextView) dialog.findViewById(R.id.bad_screenshots_software);
+                tmp = (TextView) dialog.findViewById(R.id.bad_screenshots_hardware);
+
+                tmp = (TextView) dialog.findViewById(R.id.warnings_software);
+                tmp = (TextView) dialog.findViewById(R.id.warnings_hardware);
+
+                tmp = (TextView) dialog.findViewById(R.id.crashes_software);
+                tmp = (TextView) dialog.findViewById(R.id.crashes_hardware);
+
+                ((Button) dialog.findViewById(R.id.detail_dismiss)).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
             }
         });
 
