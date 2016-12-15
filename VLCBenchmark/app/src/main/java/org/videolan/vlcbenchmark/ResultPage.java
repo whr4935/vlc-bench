6de92f8ade91c54e5907d5e52d77b0b2f4e4e20b
Duncan McNamara <dcn.mcnamara@gmail.com>
2016-12-15 17:56:51 +0100

    results: graphic redesign

--- VLCBenchmark/app/src/main/java/org/videolan/vlcbenchmark/ResultPage.java
+++ VLCBenchmark/app/src/main/java/org/videolan/vlcbenchmark/ResultPage.java
@@ -22,15 +22,21 @@ package org.videolan.vlcbenchmark;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.AppCompatCheckBox;
+import android.support.v7.widget.*;
+import android.support.v7.widget.DividerItemDecoration;
+import android.text.Layout;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -44,30 +50,21 @@ import java.util.ArrayList;
 
 public class ResultPage extends AppCompatActivity {//FragmentActivity {
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.Adapter mAdapter;
+    protected ArrayList<TestInfo>[] results;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_page);
 
-        setupUi();
-
-        ArrayList<TestInfo>[] results = (ArrayList<TestInfo>[]) getIntent().getSerializableExtra("resultsTest");
+        results = (ArrayList<TestInfo>[]) getIntent().getSerializableExtra("resultsTest");
         double soft = getIntent().getDoubleExtra("soft", 0f);
         double hard = getIntent().getDoubleExtra("hard", 0f);
 
-        final FragmentTabHost mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
-        mTabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
-
-        int index = 0;
-        for (ArrayList<TestInfo> resultList : results) {
-            for (TestInfo info : resultList) {
-                info.display();
-            }
-            Bundle args = new Bundle();
-            args.putSerializable("results", resultList);
-            mTabHost.addTab(mTabHost.newTabSpec("tab" + index).setIndicator("Test number " + (index + 1)), GridFragment.class, args);
-            index++;
-        }
+        setupUi(results[0]);
 
         TextView softView = (TextView) findViewById(R.id.softAvg);
         String softText = "Software score : " + soft;
@@ -79,7 +76,40 @@ public class ResultPage extends AppCompatActivity {//FragmentActivity {
 
     }
 
-    private void setupUi() {
+    private void setupUi(ArrayList<TestInfo> results) {
+
+        if (getIntent().hasExtra("name")) {
+            getSupportActionBar().setTitle(getIntent().getStringExtra("name"));
+        }
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.test_result_list);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView .setLayoutManager(mLayoutManager);
+
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+
+        mAdapter = new TestResultListAdapter(results);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(this, mRecyclerView, new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+//                        TestInfo test = results.get(position);
+//                        Intent intent = new Intent(getParent(), ResultDetailPage.class);
+//                        intent.putExtra("result", test);
+//                        startActivity(intent);
+                        Log.e("VLCBench", "onItemClick called");
+                    }
+
+                    @Override
+                    public void onLongItemClick(View view, int position) {
+                        Log.e("VLCBench", "onLongItemClick called");
+                    }
+                })
+        );
+
         try {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         } catch (NullPointerException e) {
@@ -97,14 +127,53 @@ public class ResultPage extends AppCompatActivity {//FragmentActivity {
         return super.onOptionsItemSelected(item);
     }
 
-//    @Override
-//    public void onBackPressed() {
-//        new AlertDialog.Builder(this).setTitle("Are you sure?").setMessage("Are you sure you want to go back?\nYou'll have no way to come back here.")
-//                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialogInterface, int i) {
-//                        ResultPage.super.onBackPressed();
-//                    }
-//                }).setNegativeButton(android.R.string.no, null).show();
-//    }
+    public class TestResultListAdapter extends RecyclerView.Adapter<TestResultListAdapter.ViewHolder> {
+        ArrayList<TestInfo> mData;
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView title;
+            public TextView hardware;
+            public TextView software;
+
+            public ViewHolder(View view) {
+                super(view);
+
+                title = (TextView) view.findViewById(R.id.test_name);
+                title.setTextSize(16);
+                hardware = (TextView) view.findViewById(R.id.test_hardware);
+                hardware.setTextSize(11);
+                software = (TextView) view.findViewById(R.id.test_software);
+                software.setTextSize(11);
+            }
+        }
+
+        public TestResultListAdapter(ArrayList<TestInfo> data) {
+            Log.e("VLCBench", "in TestResultListAdapter()");
+            for (TestInfo info : data) {
+                Log.e("VLCBench", "test name is " + info.getName());
+            }
+            mData = data;
+        }
+
+        @Override
+        public TestResultListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.test_list_rows, parent, false);
+            Log.e("VLCBench", "onCreateViewHolder");
+            return new TestResultListAdapter.ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            Log.e("VLCBench", "onBindViewHolder");
+            holder.title.setText(mData.get(position).getName());
+            holder.hardware.setText(String.valueOf("Hardware: " + mData.get(position).getHardware()));
+            holder.software.setText(String.valueOf("Software: " + mData.get(position).getSoftware()));
+        }
+
+        @Override
+        public int getItemCount() {
+            Log.e("VLCBench", "size = " + mData.size());
+            return mData.size();
+        }
+    }
 }
