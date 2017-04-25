a0b203a7c22451947bb8b7d6fed80f95b3f26253
Duncan McNamara <dcn.mcnamara@gmail.com>
2017-04-25 13:47:33 +0200

    RecyclerItemClickListener: cleanup

--- VLCBenchmark/app/src/main/java/org/videolan/vlcbenchmark/RecyclerItemClickListener.java
+++ VLCBenchmark/app/src/main/java/org/videolan/vlcbenchmark/RecyclerItemClickListener.java
@@ -26,29 +26,27 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 
-public class RecyclerItemClickListener implements RecyclerView.OnItemTouchListener {
+class RecyclerItemClickListener implements RecyclerView.OnItemTouchListener {
     private OnItemClickListener mListener;
 
-    public interface OnItemClickListener {
-        public void onItemClick(View view, int position);
-        public void onLongItemClick(View view, int position);
+    interface OnItemClickListener {
+        void onItemClick(View view, int position);
+        void onLongItemClick(View view, int position);
     }
 
-    GestureDetector mGestureDetector;
+    private GestureDetector mGestureDetector;
 
-    public RecyclerItemClickListener(Context context, final RecyclerView recyclerView,
+    RecyclerItemClickListener(Context context, final RecyclerView recyclerView,
                                      OnItemClickListener listener) {
         mListener = listener;
         mGestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
             @Override
             public boolean onSingleTapUp(MotionEvent e) {
-//                return super.onSingleTapUp(e);
                 return true;
             }
 
             @Override
             public void onLongPress(MotionEvent e) {
-//                super.onLongPress(e);
                 View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
                 if (child != null && mListener != null) {
                     mListener.onLongItemClick(child, recyclerView.getChildAdapterPosition(child));
