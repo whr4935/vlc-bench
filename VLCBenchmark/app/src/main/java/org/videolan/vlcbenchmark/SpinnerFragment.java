11e21f5bded43d123c6bd1c66a19a8622ed3427e
Duncan McNamara <dcn.mcnamara@gmail.com>
2017-07-01 15:35:23 +0200

    add loading spinner while checking files

---
+++ VLCBenchmark/app/src/main/java/org/videolan/vlcbenchmark/SpinnerFragment.java
@@ -0,0 +1,22 @@
+package org.videolan.vlcbenchmark;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Loading spinner fragment
+ */
+public class SpinnerFragment extends Fragment {
+
+    public SpinnerFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_spinner, container, false);
+    }
+}
